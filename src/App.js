7fa7d8938b67761a5c70387e74a08561f254a24c
John Brown <jakb2@cam.ac.uk>
2022-10-27 23:01:57 +0100

    Initial exploration of react
    learning about fragments, components, props, state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return(
+    <>
+      <h1>Name: {props.name}</h1>
+      <h1>Last Name: {props.lastname}</h1>
+      <h1>Age: {props.age}</h1>
+    </>
+  )
+}
+
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const name = 'John';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+
+      <Person name={"John"} lastname={"Doe"} age={30}/>
+      <Person name={"Mary"} lastname={"Doe"} age={24}/>
+      <h1>Hello, {2 + 2}!</h1>
+      {name ? (
+        <>
+          {name}
+        </>
+        
+      ) : (
+        <>
+          <h1>Test</h1>
+          <h2>there is no name</h2>
+        </>
+      )
+      }
     </div>
   );
 }
